68947e30efd7762cbec579f688020aa7e802e3c3
Matheus Siqueira <matheuspontessiqueira@gmail.com>
2022-12-06 03:34:01 -0300

    Receitas_Supremas-Django

--- receitas/views/receita.py
+++ receitas/views/receita.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
-from .models import Receita
+from receitas.models import Receita
 from django.contrib.auth.models import User
 
 
@@ -20,20 +20,6 @@ def receita(request, receita_id):
         'receita' : receita
     }
     return render(request, 'receitas/receita.html', receita_a_exibir)
-
-def buscar(request):
-
-    lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
-
-    if 'buscar' in request.GET:
-        nome_a_buscar = request.GET['buscar']
-        if buscar:
-            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
-    dados = {
-        'receitas' : lista_receitas
-    }
-
-    return render(request, 'receitas/buscar.html', dados)
 
 def cria_receita(request):
     if request.method == 'POST':
